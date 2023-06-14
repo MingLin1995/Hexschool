888089a2d56ce28c01f7b4d36a7f217599a0a761
Ming <125284928+MingLin1995@users.noreply.github.com>
2023-06-14 19:46:01 +0800

    Add files via upload

--- 工程師體驗營/JS/event.js
+++ 工程師體驗營/JS/event.js
@@ -32,4 +32,55 @@ elAdd.addEventListener("click",function(){alert("add-2")},false);
 
 
 /* ------------------------------ */
-/* Event Bubbling、Event Capturing */
+/* false（事件氣泡-Event Bubbling）：從指定元素往外找（預設） */
+/* ture（事件捕捉-Event Capturing）：從最層往內找到指定元素 */
+
+var el3=document.querySelector(".box");
+el3.addEventListener("click",function(){
+    alert("點到box");
+    console.log("印出box");
+},false); /* 跟body重疊，所以也會觸發body的動作 */
+
+var el4=document.querySelector(".body");
+el4.addEventListener("click",function(){
+    alert("點到body");
+    console.log("印出body");
+},false);
+
+
+/* ------------------------------ */
+/* stopPropagation - 中止冒泡事件(只執行一次，其他都不執行) */
+var el5=document.querySelector(".box2");
+el5.addEventListener("click",function(e){
+    e.stopPropagation(); /* 就不會往上找上一層的東西 */
+    alert("點到box");
+    console.log("印出box");
+},false);
+
+
+/* ------------------------------ */
+/* preventDefault - 取消預設觸發行為(取消默認行為) */
+var el6=document.querySelector(".link");
+el6.addEventListener("click",function(e){
+    /* 取消元素的默認行為，原本點連結會轉跳到指定網頁(還是會跳出點擊到BODY) */
+    e.preventDefault();
+    /* 只執行指定元素，其他行為都不執行(不會跳出點擊到BODY)*/
+    e.stopPropagation();
+    console.log("test");
+},false);
+
+/* ------------------------------ */
+/* e.target - 了解目前所在元素位置 */
+/* 是點到ul、li還是a標籤? */
+var el7=document.querySelector(".header");
+el7.addEventListener("click",function(e){
+    e.preventDefault(); /* 不會跳到最上面 */
+    e.stopPropagation(); /* 不會點到BODY */
+    console.log(e.target); /* target可以從e裡面去查詢 */
+    console.log(e.target.nodeName); /* 可以從target裡面去查詢 */
+},false);
+
+
+/* ------------------------------ */
+/* change - 表單內容更動內容時觸發 */
+
