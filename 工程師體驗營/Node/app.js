c9301fcc1243a5991cc568d8acc2f76c308b3672
Ming <125284928+MingLin1995@users.noreply.github.com>
2023-06-28 22:10:40 +0800

    Add files via upload

---
+++ 工程師體驗營/Node/app.js
@@ -0,0 +1,10 @@
+/* 載入模組到相對應的路徑 */
+var content=require("./data");
+/* ./代表同一層 ../代表上一層 */
+
+
+var a=1;
+console.log(a);
+
+console.log(content);
+console.log(content.title);
---
+++ 工程師體驗營/Node/data.js
@@ -0,0 +1,10 @@
+var data=2;
+
+/* 指定哪些資料要被載入 */
+module.exports=data;
+
+/* 也可以提供物件 */
+module.exports={ 
+    content:data,
+    title:"title"
+};
