af67e6949b1f30aceaaa46b388c80b828fbd7cc6
Ming <125284928+MingLin1995@users.noreply.github.com>
2023-06-29 21:32:01 +0800

    Add files via upload

--- 工程師體驗營/Node/app.js
+++ 工程師體驗營/Node/app.js
@@ -1,10 +1,15 @@
 /* 載入模組到相對應的路徑 */
 var content=require("./data");
 /* ./代表同一層 ../代表上一層 */
+/* ------------------------ */
 
-
-var a=1;
-console.log(a);
-
+/* 第一種載入方法 */
 console.log(content);
+/* 載入物件 */
 console.log(content.title);
+/* ------------------------ */
+
+/* 第二種載入方法 */
+/* console.log(content.data2); */
+
+/* console.log(content.bark()); */
--- 工程師體驗營/Node/data.js
+++ 工程師體驗營/Node/data.js
@@ -1,10 +1,21 @@
-var data=2;
+var data1=2;
 
-/* 指定哪些資料要被載入 */
-module.exports=data;
+/* 第一種載入方法 */
+/*module.exports=data1;*/
 
-/* 也可以提供物件 */
+/* 也可以提供物件供載入 */
 module.exports={ 
-    content:data,
+    content:data1,
     title:"title"
 };
+/* ------------------------ */
+
+/* 第二種載入方法 */
+/* exports.data2=22; */
+
+/* 載入function */
+/*exports.bark=function(){
+    return "bark!!";
+}*/
+
+/* 第一種方法跟第二種方法不能共存！！！ */
